17cb93b61ae6866ef7b5e02d23f967e17eb73908
Clement Habinshuti <hello@habbes.xyz>
2017-05-07 00:35:45 +0300

    Remove unused imports

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { generateId } from './TodoService';
 import { addTodo } from './actions';
 import TodoInput from './TodoInput';
 
