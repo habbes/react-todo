d3ecac61383475a01abf1b7216b292d56d49496c
Clement Habinshuti <hello@habbes.xyz>
2017-05-07 00:04:19 +0300

    Use connect to generate AddTodo container

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { generateId } from './TodoService';
 import TodoInput from './TodoInput';
 
-export default function AddTodo (props, {store}) {
-    return (
-        <TodoInput onAddTodo={text => 
-            store.dispatch({
+const AddTodo = connect(null, mapDispatchToProps)(TodoInput);
+export default AddTodo;
+
+function mapDispatchToProps (dispatch) {
+    return {
+        onAddTodo: text => {
+            dispatch({
                 type: 'ADD_TODO',
                 id: generateId(),
                 text
-            })} />
-    );
+            });
+        }
+    }
 }
-
-AddTodo.contextTypes = {
-    store: PropTypes.object
-};
