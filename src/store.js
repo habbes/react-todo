9bece926ecef50522f2f09ee520efb84b28fd067
Clement Habinshuti <hello@habbes.xyz>
2017-05-09 10:04:07 +0300

    throttle calls to saveState

--- src/store.js
+++ src/store.js
@@ -1,13 +1,18 @@
 import { createStore } from 'redux';
+import throttle from 'lodash/throttle';
 import reducer from './services/reducer';
 import { loadState, saveState } from './services/local-storage';
 
 const persistedState = loadState();
 const store = createStore(reducer, persistedState);
-store.subscribe(() => {
+
+// throttle the call to saveState so it's called at most once
+// per second to avoid to made invocation since the save state
+// is exponsive due to JSON serialization
+store.subscribe(throttle(()=> {
     saveState({
         todos: store.getState().todos
     })
-});
+}, 1000));
 
 export default store;
