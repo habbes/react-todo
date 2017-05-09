58dbf7e850bc18160da5d6d1fa715c78e318f1a4
Clement Habinshuti <hello@habbes.xyz>
2017-05-09 10:00:41 +0300

    Use uuid to generate todo ids

--- src/services/todo.js
+++ src/services/todo.js
@@ -1,18 +1,4 @@
-let nextId = 1;
-
-export function generateId () {
-    return nextId++;
-}
-
-export function createTodo (text) {
-    const id = nextId;
-    nextId += 1;
-    return {
-        id,
-        text,
-        completed: false
-    };
-}
+export { v4 as generateId } from 'node-uuid';
 
 export function filterTodos (todos, filter) {
     return todos.filter(t => 
