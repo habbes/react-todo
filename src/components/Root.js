3bda39228ddc5ec4f7fe27bb22859492ef05156c
Clement Habinshuti <hello@habbes.xyz>
2017-05-15 22:06:52 +0300

    Add react router dom

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 
 export default function Root ({ store }) {
     return (
         <Provider store={ store }>
-            <App/>
+            <Router>
+                <Route path="/" component={ App } />
+            </Router>
         </Provider>
     );
 }
